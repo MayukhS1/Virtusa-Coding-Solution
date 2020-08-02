4561d4f26c032ae74dc1777f8a0e2f745aebe44c
Mayukh Sasmal <49561618+MayukhS1@users.noreply.github.com>
2020-08-02 22:44:08 +0530

    Create Pairs.java

---
+++ Pairs.java
@@ -0,0 +1,56 @@
+//time limit exceeded 
+//9/14 cases run
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pairs function below. Main logic is here ... better version will be uploaded
+    static int pairs(int k, int[] arr) {
+        int count =0;
+        for(int val1: arr){
+            for(int val2: arr){
+                if(val1-val2 == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
