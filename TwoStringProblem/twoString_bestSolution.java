e047c499271856f089a45b13edc80a3788e99b6b
Mayukh Sasmal <49561618+MayukhS1@users.noreply.github.com>
2020-08-03 21:22:21 +0530

    Create twoString_bestSolution.java

---
+++ TwoStringProblem/twoString_bestSolution.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below. Main logic 
+    static String twoStrings(String s1, String s2) {
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        for(char ch: str.toCharArray()){
+            if(s1.indexOf(ch)>=0 && s2.indexOf(ch)>=0)
+                return "YES";
+        }
+        return "NO";
+        
+    }
+    //taking input
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
